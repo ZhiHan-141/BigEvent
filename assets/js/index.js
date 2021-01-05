a0bf248abdc03e916055cbe9f8c38664edbc75a3
chen <435466122@qq.com>
2021-01-05 10:07:33 +0800

    主页完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
 $(function () {
     //页面刷新就获取用户信息
+    // 调用函数获取
+    getUserInfo();
+})
+// 获取用户信息函数
+function getUserInfo() {
     //从本地获取
     var token = localStorage.getItem('token')
     $.ajax({
@@ -10,7 +15,24 @@ $(function () {
             Authorization: token || ''
         },
         success: function (res) {
-                console.log(res);
+            console.log(res);
+            console.log(res.data.user_pic);
+            // 调用渲染头像函数
+            renderAvatar(res.data)
         }
     })
-})
+}
+function renderAvatar(user) {
+    //获取用户名
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp&nbsp' + name)
+    //设置用户头像
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
